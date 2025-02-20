6d40ea5010a0595be0392f630ff5596e98e7acaf
panbotsjsj <iqbalgaming641@gmail.com>
2025-02-20 08:39:34 +0700

    Update T2.java

--- Minggu3/Minggu3_2/src/minggu3_2/T2.java
+++ Minggu3/Minggu3_2/src/minggu3_2/T2.java
@@ -12,26 +12,18 @@ import java.util.Scanner;
 import java.util.Random;
 public class T2 {
     public static void main(String[] args) {
-        int baris,kolom;
-        
         Scanner scan = new Scanner(System.in);
         Random random = new Random();       
                 
-        System.out.print("Masukkan Jumlah Barisnya Sayang : ");
-        baris = scan.nextInt();
-        System.out.print("Masukkan Jumlah Kolomnya Sayang : ");
-        kolom = scan.nextInt();
+        System.out.print("Masukkan Panjang Deretnya Sayang : ");
+        int deret = scan.nextInt();
         
-        int[][] deret = new int [baris][kolom];
+        int [] hasil = new int [deret];
         
-        for (int i=0; i<baris; i++){
-            for (int j=0; j<kolom; j++){
-                deret[i][j] = random.nextInt(100);
-                System.out.print(deret[i][j]+" ");
+        for (int i=0; i<deret; i++){
+                hasil[i] = random.nextInt(100);
+                System.out.print(hasil[i]+" ");
             }
         System.out.println("");    
-        }
-        
-        
     }
 }
